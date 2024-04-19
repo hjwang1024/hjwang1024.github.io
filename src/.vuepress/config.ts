a905a3b7cbcac6af15db10c738598082e4f63f8c
wanghongjie <95022304+hjwang1024@users.noreply.github.com>
2024-04-19 14:52:18 +0800

    添加播放器

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from 'vuepress'
 import theme from './theme.js'
+import metingPlugin from 'vuepress-plugin-meting2'
 
 export default defineUserConfig({
     base: '/',
@@ -11,4 +12,31 @@ export default defineUserConfig({
 
     // 和 PWA 一起启用
     // shouldPrefetch: false,
+    plugins: [
+        metingPlugin({
+            metingOptions: {
+                global: true,
+                server: 'tencent',
+                api: 'https://api.injahow.cn/meting/?server=:server&type=:type&id=:id&auth=:auth&r=:r',
+                type: 'playlist',
+                mid: '851947617',
+            },
+        }),
+    ],
+    // plugins: [
+    //     metingPlugin,
+    //     {
+    //         // 这个 API 是不可用的，只是作为示例而已
+    //         metingApi: 'https://api.injahow.cn/meting/?server=:server&type=:type&id=:id&auth=:auth&r=:r',
+    //         meting: {
+    //             server: 'tencent',
+    //             type: 'playlist',
+    //             mid: '851947617',
+    //         }, // 不配置该项的话不会出现全局播放器
+    //         aplayer: {
+    //             lrcType: 3,
+    //         },
+    //         defaultCover: 'https://nyakku.moe/avatar.jpg',
+    //     },
+    // ],
 })
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -71,6 +71,17 @@ export default hopeTheme({
         searchPro: {
             // 索引全部内容
             indexContent: true,
+            // 为分类和标签添加索引
+            customFields: [
+                {
+                    getter: page => page.frontmatter.category as any,
+                    formatter: '分类：$content',
+                },
+                {
+                    getter: page => page.frontmatter.tag as any,
+                    formatter: '标签：$content',
+                },
+            ],
         },
         comment: {
             provider: 'Giscus', // Artalk | Giscus | Waline | Twikoo
