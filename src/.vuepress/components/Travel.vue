22c159ef94fad9fe2dc2168cf1781e1134b0cacd
wanghongjie <95022304+hjwang1024@users.noreply.github.com>
2024-04-26 16:19:31 +0800

    Update Travel.vue

--- src/.vuepress/components/Travel.vue
+++ src/.vuepress/components/Travel.vue
@@ -2,8 +2,7 @@
     <div class="img-container">
         <img
             v-for="(item, index) in list"
-            :data-src="item"
-            src="/images/travel-background.jpeg"
+            :src="item"
             :key="index"
             @click="openPhotoSwipe(index)"
             alt="item"
@@ -21,38 +20,38 @@ import { createPhotoSwipe } from '@vuepress/plugin-photo-swipe/client'
 let list = Object.values((import.meta as any).glob('../public/images/travel/*.*', { eager: true })).map(
     (v: any) => v.default
 )
-let state: any = null
+// let state: any = null
 
-const openPhotoSwipe = (index: number) => {
-    console.log(index)
-}
+// const openPhotoSwipe = (index: number) => {
+//     console.log(index)
+// }
 
-onMounted(async () => {
-    // 通过图片链接创建一个新的 photoswipe 实例
-    state = await createPhotoSwipe(list, {
-        // photoswipe 选项
-    })
-    const option = {
-        root: null, //所监听对象的具体祖先元素。如果未传入值或值为null，则默认使用顶级文档的视窗。
-        rootMargin: '20px 0px', //预先加载的距离
-        threshold: [0], // 特定的相交比例
-    }
-    const images = document.querySelectorAll('.img-item')
-    const intersectionObserver = new IntersectionObserver(callback, option)
-    function callback(entries) {
-        entries.forEach(item => {
-            if (item.isIntersecting) {
-                item.target.src = item.target.dataset.src // 开始加载图片,把data-origin的值放到src
-                intersectionObserver.unobserve(item.target) // 停止监听已开始加载的图片
-            }
-        })
-    }
-    images.forEach(item => intersectionObserver.observe(item))
-})
+// onMounted(async () => {
+//     // 通过图片链接创建一个新的 photoswipe 实例
+//     state = await createPhotoSwipe(list, {
+//         // photoswipe 选项
+//     })
+//     const option = {
+//         root: null, //所监听对象的具体祖先元素。如果未传入值或值为null，则默认使用顶级文档的视窗。
+//         rootMargin: '20px 0px', //预先加载的距离
+//         threshold: [0], // 特定的相交比例
+//     }
+//     const images = document.querySelectorAll('.img-item')
+//     const intersectionObserver = new IntersectionObserver(callback, option)
+//     function callback(entries) {
+//         entries.forEach(item => {
+//             if (item.isIntersecting) {
+//                 item.target.src = item.target.dataset.src // 开始加载图片,把data-origin的值放到src
+//                 intersectionObserver.unobserve(item.target) // 停止监听已开始加载的图片
+//             }
+//         })
+//     }
+//     images.forEach(item => intersectionObserver.observe(item))
+// })
 
-onUnmounted(() => {
-    state?.destroy()
-})
+// onUnmounted(() => {
+//     state?.destroy()
+// })
 </script>
 
 <style lang="scss">
